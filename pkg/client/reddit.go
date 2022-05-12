d17f45b8dbd887f44d7336c307bee1b6666b3db0
Max Lantas <mnxndev@outlook.com>
2022-05-12 10:51:28 -0700

    return text/xml content type

--- pkg/client/reddit.go
+++ pkg/client/reddit.go
@@ -137,7 +137,7 @@ func RssHandler(redditURL string, now NowFn, client *http.Client, getArticle Get
 		http.Error(w, err.Error(), 500)
 	}
 
-	w.Header().Set("Content-Type", "application/rss+xml")
+	w.Header().Set("Content-Type", "text/xml")
 	w.Header().Set("Cache-Control", "s-maxage=1800, stale-while-revalidate=3600")
 	io.WriteString(w, rss)
 }
